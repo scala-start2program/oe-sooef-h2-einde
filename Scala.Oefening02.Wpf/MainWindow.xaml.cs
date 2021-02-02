f61159f6631b1e411d95e9bf4ce2209a5b18be7e
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-02-02 14:20:14 +0100

    Add project files.

---
+++ Scala.Oefening02.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Scala.Oefening02.Core;
+
+namespace Scala.Oefening02.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Fiets fiets1 = new Fiets("Oxford", "OX120", 879.23M, false, 2);
+            Fiets fiets2 = new Fiets("Oxford", "OX240", 1245.3M, false, 2);
+            Fiets fiets3 = new Fiets("Oxford", "iOX120", 2879.99M, true, 2);
+            Fiets fiets4 = new Fiets("Cambridge", "SNR1900", 546M, false, 3);
+
+            lstFietsen.Items.Add(fiets1);
+            lstFietsen.Items.Add(fiets2);
+            lstFietsen.Items.Add(fiets3);
+            lstFietsen.Items.Add(fiets4);
+        }
+
+        private void lstFietsen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Fiets fiets = (Fiets)lstFietsen.SelectedItem;
+            lblMerk.Content = fiets.Merk;
+            lblSerie.Content = fiets.Serie;
+            lblVerkoopprijsEuro.Content = fiets.VerkoopprijsEuro.ToString("#,##0.00 €");
+            lblVerkoopprijsPond.Content = fiets.GetVerkoopprijsAndereMunt(0.88).ToString("#,##0.00 £");
+            lblVerkoopprijsDollar.Content = fiets.GetVerkoopprijsAndereMunt(1.2).ToString("#,##0.00 $");
+            lblElektrisch.Content = "Neen";
+            if (fiets.IsElektrisch) lblElektrisch.Content = "Ja";
+            lblAantalWielen.Content = fiets.AantalWielen;
+        }
+    }
+}
---
+++ Scala.oefening02.Core/Fiets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scala.Oefening02.Core
+{
+    public class Fiets
+    {
+        public string Merk { get; set; }
+        public string Serie { get; set; }
+        public decimal VerkoopprijsEuro { get; set; }
+        public bool IsElektrisch { get; set; }
+        public byte AantalWielen { get; set; }
+
+        public Fiets()
+        {
+        }
+        public Fiets(string merk, string serie, decimal verkoopprijsEuro, bool isElektrisch, byte aantalWielen)
+        {
+            Merk = merk;
+            Serie = serie;
+            VerkoopprijsEuro = verkoopprijsEuro;
+            IsElektrisch = isElektrisch;
+            AantalWielen = aantalWielen;
+        }
+        public override string ToString()
+        {
+            return $"{Merk} - {Serie}";
+        }
+        public decimal GetVerkoopprijsAndereMunt(double wisselkoers)
+        {
+            return VerkoopprijsEuro * (decimal)wisselkoers;
+
+            // £ = 0.88;
+            // $ = 1.2;
+        }
+
+    }
+}
